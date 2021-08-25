67af8da11f41b39db83ecb6bf8f6b0c88a72c1df
Josef Nemec <nemecjosef@gmail.com>
2021-08-25 16:46:19 +0200

    Localization updates

---
+++ build/UpdateLocalizations.ps1
@@ -0,0 +1,59 @@
+param(
+    [string]$AccessToken
+)
+
+$ErrorActionPreference = "Stop"
+Add-Type -AssemblyName "PresentationFramework"
+
+$locDir = Join-Path $pwd "..\source\Localization"
+$urlRoot = "https://crowdin.com/api/v2"
+$playnitePrjId = 345875
+$requestHeaders = @{
+    "Authorization" = "Bearer $AccessToken"
+}
+
+$locProgressData = @{}
+$locProgress = Invoke-RestMethod -Headers $requestHeaders -Uri "$urlRoot/projects/$playnitePrjId/languages/progress?limit=100"
+foreach ($lng in $locProgress.data)
+{
+    if ($lng.data.languageId -eq "en")
+    {
+        continue
+    }
+
+    $locProgressData.Add($lng.data.languageId, $lng.data.translationProgress);
+    $locDownloadData = Invoke-RestMethod -Method Post -Headers $requestHeaders -Uri "$urlRoot/projects/$playnitePrjId/translations/builds/files/33" `
+                                         -Body "{`"targetLanguageId`":`"$($lng.data.languageId)`"}" -ContentType "application/json"
+    
+    $locDownload = Invoke-WebRequest -Uri $locDownloadData.data.url
+    $locDownload.Headers["Content-Disposition"][0] -match '"(.+)"' | Out-Null
+    $fileName = $Matches[1]
+    [System.IO.File]::WriteAllBytes((Join-Path $locDir $fileName), $locDownload.Content)
+}
+
+$allOk = $true
+foreach ($locFile in (Get-ChildItem $locDir -Filter "*.xaml"))
+{
+    $stream = New-Object "System.IO.StreamReader" $locFile.FullName
+
+    try
+    {
+        $xaml = [System.Windows.Markup.XamlReader]::Load($stream.BaseStream)
+        Write-Host "$($locFile.Name)...OK" -ForegroundColor Green
+    }
+    catch
+    {
+        $allOk = $false
+        Write-Host "$($locFile.Name)...FAIL" -ForegroundColor Red
+        Write-Host $_.Exception.InnerException.Message -ForegroundColor Red
+    }
+    finally
+    {
+        $stream.Dispose()
+    }
+}
+
+if (-not $allOk)
+{
+    throw "Some localization files failed verification."
+}
