da9b0a644a0897c4ffa78e18370befdcc60a8600
Brandon <brandon_corchado1@hotmail.com>
2021-10-27 10:51:55 -0500

    Update EpicLauncher.cs

--- source/Libraries/EpicLibrary/EpicLauncher.cs
+++ source/Libraries/EpicLibrary/EpicLauncher.cs
@@ -50,7 +50,7 @@ namespace EpicLibrary
                     // Try default location. These registry keys sometimes go missing on people's PCs...
                     if (File.Exists(GetExecutablePath(@"C:\Program Files (x86)\Epic Games\")))
                     {
-                        return @"C:\Program Files(x86)\Epic Games\";
+                        return @"C:\Program Files (x86)\Epic Games\";
                     }
                     else if (File.Exists(GetExecutablePath(@"C:\Program Files\Epic Games\")))
                     {
