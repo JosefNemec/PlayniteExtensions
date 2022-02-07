557e036d07cea5a153261ebfd36755f53e237575
Brandon <brandon_corchado1@hotmail.com>
2022-02-07 06:02:03 -0600

    Fix setting install directory if it doesn't exists

--- source/Libraries/EpicLibrary/EpicLibrary.cs
+++ source/Libraries/EpicLibrary/EpicLibrary.cs
@@ -69,6 +69,7 @@ namespace EpicLibrary
                 {
                     logger.Info($"Epic game {gameName} installation directory {installLocation} not detected.");
                     isInstalled = false;
+                    installLocation = string.Empty;
                 }
 
                 var game = new GameMetadata()
