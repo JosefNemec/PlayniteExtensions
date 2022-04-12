a6081afcad948b4ebe78e831bc36e074293cde8e
Josef Nemec <nemecjosef@gmail.com>
2022-04-12 16:34:52 +0200

    Epic: Better error reporting when EGS is down #134

--- source/Libraries/EpicLibrary/Services/EpicAccountClient.cs
+++ source/Libraries/EpicLibrary/Services/EpicAccountClient.cs
@@ -284,7 +284,16 @@ namespace EpicLibrary.Services
                 }
                 else
                 {
-                    return new Tuple<string, T>(str, Serialization.FromJson<T>(str));
+                    try
+                    {
+                        return new Tuple<string, T>(str, Serialization.FromJson<T>(str));
+                    }
+                    catch
+                    {
+                        // For cases like #134, where the entire service is down and doesn't even return valid error messages.
+                        logger.Error(str);
+                        throw new Exception("Failed to get data from Epic service.");
+                    }
                 }
             }
         }
