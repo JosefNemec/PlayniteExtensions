6e915568af2e90d3c7b6687ec8dd748cfc8effa6
Dawid Karczewski <dawidxen@gmail.com>
2022-11-22 14:57:11 +0100

    Cleans up Ubisoft Connect naming

--- source/Libraries/UplayLibrary/Properties/AssemblyInfo.cs
+++ source/Libraries/UplayLibrary/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UplayLibrary")]
-[assembly: AssemblyDescription("Uplay library support plugin for Playnite")]
+[assembly: AssemblyDescription("Ubisoft Connect library support plugin for Playnite")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UplayLibrary")]
