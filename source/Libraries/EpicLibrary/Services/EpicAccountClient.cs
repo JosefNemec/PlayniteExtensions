776a3827c52d19957dd70023d470382d8b732568
Josef Nemec <nemecjosef@gmail.com>
2022-12-07 10:26:03 +0100

    Updated user agent for Epic plugin

--- source/Libraries/EpicLibrary/Services/EpicAccountClient.cs
+++ source/Libraries/EpicLibrary/Services/EpicAccountClient.cs
@@ -44,7 +44,7 @@ namespace EpicLibrary.Services
         private readonly string catalogUrl = @"";
         private readonly string playtimeUrl = @"";
         private const string authEncodedString = "MzRhMDJjZjhmNDQxNGUyOWIxNTkyMTg3NmRhMzZmOWE6ZGFhZmJjY2M3Mzc3NDUwMzlkZmZlNTNkOTRmYzc2Y2Y=";
-        private const string userAgent = @"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Vivaldi/4.3";
+        private const string userAgent = @"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Vivaldi/5.5.2805.50";
 
         public EpicAccountClient(IPlayniteAPI api, string tokensPath)
         {
