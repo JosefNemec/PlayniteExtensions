bbb7cd4c2ce65625e1f10dd830f36ce6ccf783ae
Jeshibu <jeshibu@gmail.com>
2023-02-02 01:17:43 +0100

    Steam: Removed dialog focusing because it's unnecessary since the latest Steam update

--- source/Libraries/SteamLibrary/SteamGameController.cs
+++ source/Libraries/SteamLibrary/SteamGameController.cs
@@ -1,18 +1,13 @@
-﻿using Playnite;
-using Playnite.Common;
+﻿using Playnite.Common;
 using Playnite.SDK;
 using Playnite.SDK.Models;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using SteamKit2;
 using System.IO;
 using Playnite.SDK.Plugins;
-using System.Runtime.InteropServices;
 
 namespace SteamLibrary
 {
@@ -180,7 +175,6 @@ namespace SteamLibrary
                 ))
             {
                 ProcessStarter.StartUrl($"steam://launch/{Game.GameId}/Dialog");
-                Task.Run(FocusDialogWindow); //Don't block the main thread with the startup wait
             }
             else
             {
@@ -211,110 +205,5 @@ namespace SteamLibrary
             stopWatch?.Stop();
             InvokeOnStopped(new GameStoppedEventArgs(Convert.ToUInt64(stopWatch?.Elapsed.TotalSeconds ?? 0)));
         }
-
-        private void FocusDialogWindow()
-        {
-            const int timeoutSeconds = 4;
-            try
-            {
-                // Get Steam's process ID for comparison with child process parent IDs
-                var steamProcess = Process.GetProcessesByName("steam")?.FirstOrDefault();
-                if (steamProcess == null)
-                {
-                    logger.Trace("Steam isn't running. The Steam dialog window will focus without our help once Steam starts up.");
-                    return;
-                }
-
-                var windows = new List<WindowInfo>();
-                var stopwatch = Stopwatch.StartNew();
-
-                do
-                {
-                    // Wait for dialog window to be created
-                    Thread.Sleep(200);
-
-                    windows = GetSteamDialogWindows(steamProcess);
-                }
-                while (windows.Count == 0 && stopwatch.ElapsedMilliseconds < timeoutSeconds * 1000);
-
-                if (windows.Count == 0)
-                {
-                    logger.Trace($"No Steam dialog windows found to focus within {timeoutSeconds} seconds");
-                    return;
-                }
-
-                // The list has the foremost items first, and we want to focus everything ending with the window that is at the front (the startup choice dialog)
-                // So run through it in reverse
-                for (int i = windows.Count - 1; i >= 0; i--)
-                {
-                    // Wait for the previous focus call to resolve
-                    // Otherwise this one might be ignored
-                    Thread.Sleep(10);
-                    var window = windows[i];
-                    logger.Debug($"Setting foreground window: {window.Handle} {window.Title}");
-                    User32.SetForegroundWindow(window.Handle);
-                }
-            }
-            catch (Exception ex)
-            {
-                logger.Warn(ex, "Error focusing Steam game startup dialog");
-            }
-        }
-
-        #region FindWindows
-
-        private class WindowInfo
-        {
-            public WindowInfo(IntPtr handle, string title)
-            {
-                Handle = handle;
-                Title = title;
-            }
-
-            public IntPtr Handle { get; set; }
-            public string Title { get; set; }
-        }
-
-        private static List<WindowInfo> GetSteamDialogWindows(Process steamProcess)
-        {
-            var windows = new List<WindowInfo>();
-
-            User32.EnumWindows(delegate (IntPtr hWnd, IntPtr lParam)
-            {
-                User32.GetWindowThreadProcessId(hWnd, out uint processId);
-                if (steamProcess.Id != processId)
-                {
-                    // Return true here so that we iterate all windows
-                    return true;
-                }
-
-                var text = GetWindowText(hWnd);
-                if (!text.EndsWith(" Steam"))
-                {
-                    return true;
-                }
-
-                windows.Add(new WindowInfo(hWnd, text));
-                return true;
-            }, IntPtr.Zero);
-
-            return windows;
-        }
-
-        /// <summary> Get the text for the window pointed to by hWnd </summary>
-        private static string GetWindowText(IntPtr hWnd)
-        {
-            int size = User32.GetWindowTextLength(hWnd);
-            if (size > 0)
-            {
-                var builder = new StringBuilder(size + 1);
-                User32.GetWindowText(hWnd, builder, builder.Capacity);
-                return builder.ToString();
-            }
-
-            return string.Empty;
-        }
-
-        #endregion FindWindows
     }
 }
