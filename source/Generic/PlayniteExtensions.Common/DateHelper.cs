5bfab3e4f3f54bf33c3dee37de0e11d761533db4
Jeshibu <jeshibu@gmail.com>
2023-03-01 17:21:51 +0100

    Steam: fixed imprecise release date parsing as precise date

---
+++ source/Generic/PlayniteExtensions.Common/DateHelper.cs
@@ -0,0 +1,49 @@
+﻿using Playnite.SDK.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayniteExtensions.Common
+{
+    public static class DateHelper
+    {
+        public static ReleaseDate? ParseReleaseDate(string dateString)
+        {
+            if (string.IsNullOrWhiteSpace(dateString))
+            {
+                return null;
+            }
+
+            var currentCulture = CultureInfo.CurrentCulture;
+            var invariantCulture = CultureInfo.InvariantCulture;
+            var dateTimeStyles = DateTimeStyles.AssumeLocal | DateTimeStyles.AllowWhiteSpaces;
+
+            DateTime parsedDate;
+
+            if (DateTime.TryParseExact(dateString, "yyyy", currentCulture, dateTimeStyles, out parsedDate)
+                || DateTime.TryParseExact(dateString, "yyyy", invariantCulture, dateTimeStyles, out parsedDate))
+            {
+                return new ReleaseDate(parsedDate.Year);
+            }
+
+            var yearAndMonthFormats = new[] { "MMM yyyy", "MMMM yyyy" };
+
+            if (DateTime.TryParseExact(dateString, yearAndMonthFormats, currentCulture, dateTimeStyles, out parsedDate)
+                || DateTime.TryParseExact(dateString, yearAndMonthFormats, invariantCulture, dateTimeStyles, out parsedDate))
+            {
+                return new ReleaseDate(parsedDate.Year, parsedDate.Month);
+            }
+
+            if (DateTime.TryParse(dateString, currentCulture, dateTimeStyles, out parsedDate)
+                || DateTime.TryParse(dateString, invariantCulture, dateTimeStyles, out parsedDate))
+            {
+                return new ReleaseDate(parsedDate);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ source/Tests/Common.Tests/DateHelperTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Playnite.SDK.Models;
+using System.Globalization;
+using PlayniteExtensions.Common;
+
+namespace Common.Tests
+{
+    [TestFixture]
+    class DateHelperTests
+    {
+        public static object[] DateParseCases =
+        {
+            new object[] { "March 2023", new ReleaseDate(2023, 3), "en-US" },
+            new object[] { "March 2023", new ReleaseDate(2023, 3), "fr-FR" },
+            new object[] { "Mars 2023", new ReleaseDate(2023, 3), "fr-FR" },
+            new object[] { "2023", new ReleaseDate(2023), "fr-FR" },
+            new object[] { "March 1, 2023", new ReleaseDate(2023, 3, 1), "en-US" },
+            new object[] { "March 1, 2023", new ReleaseDate(2023, 3, 1), "nl-NL" },
+            new object[] { "1 maart 2023", new ReleaseDate(2023, 3, 1), "nl-NL" },
+            new object[] { "Gibberish", null, "en-US" },
+            new object[] { "Gibberish", null, "nl-NL" },
+        };
+
+        [TestCaseSource(nameof(DateParseCases))]
+        public void ParseReleaseDateReturnsExpected(string input, ReleaseDate? expectedOutput, string cultureId)
+        {
+            CultureInfo.CurrentCulture = new CultureInfo(cultureId);
+            var result = DateHelper.ParseReleaseDate(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+    }
+}
