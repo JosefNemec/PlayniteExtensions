fbb6769cd65b37141d784cc999cb6288341832b0
Josef Nemec <nemecjosef@gmail.com>
2023-11-22 14:18:50 +0100

    Battle.net login not working correctly

--- source/Libraries/BattleNetLibrary/Services/BattleNetAccountClient.cs
+++ source/Libraries/BattleNetLibrary/Services/BattleNetAccountClient.cs
@@ -17,6 +17,7 @@ namespace BattleNetLibrary.Services
         private const string apiStatusUrl = @"https://account.battle.net/api/";
         private const string gamesUrl = @"https://account.battle.net/api/games-and-subs";
         private const string classicGamesUrl = @"https://account.battle.net/api/classic-games";
+        private const string defaultLogoutUri = @"https://account.battle.net:443/api/logout";
         private static ILogger logger = LogManager.GetLogger();
         private IWebView webView;
 
@@ -53,7 +54,7 @@ namespace BattleNetLibrary.Services
                 }
             };
 
-            webView.Navigate(apiUrls.logoutUri);
+            webView.Navigate(apiUrls.logoutUri ?? defaultLogoutUri);
             webView.OpenDialog();
         }
 
@@ -89,8 +90,14 @@ namespace BattleNetLibrary.Services
                         }
                     }
 
-                    var deserialized = Serialization.FromJson<BattleNetApiStatus>(responseText);
-                    return deserialized;
+                    if (Serialization.TryFromJson<BattleNetApiStatus>(responseText, out var status))
+                    {
+                        return status;
+                    }
+                    else
+                    {
+                        return new BattleNetApiStatus();
+                    }
                 }
 
                 return null;
@@ -103,7 +110,14 @@ namespace BattleNetLibrary.Services
             webView.NavigateAndWait("https://account.battle.net:443/oauth2/authorization/account-settings");
             webView.NavigateAndWait(apiStatusUrl);
             var textStatus = webView.GetPageText();
-            return Serialization.FromJson<BattleNetApiStatus>(textStatus);
+            if (Serialization.TryFromJson<BattleNetApiStatus>(textStatus, out var status))
+            {
+                return status;
+            }
+            else
+            {
+                return new BattleNetApiStatus();
+            }
         }
     }
 }
