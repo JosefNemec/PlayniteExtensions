3089415778ff845bc884d3509e44767191684844
Jeshibu <jeshibu@gmail.com>
2025-11-19 21:01:01 +0100

    Updated SteamCommunityService to derive from SteamAuthServiceBase

--- source/Libraries/SteamLibrary/Services/SteamCommunityService.cs
+++ source/Libraries/SteamLibrary/Services/SteamCommunityService.cs
@@ -1,41 +1,40 @@
 using Playnite.SDK;
 using SteamLibrary.Models;
+using SteamLibrary.Services.Base;
 using System;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace SteamLibrary.Services
 {
-    public class SteamCommunityService
+    /// <summary>
+    /// Former login provider, no longer used
+    /// </summary>
+    public class SteamCommunityService : SteamAuthServiceBase
     {
-        private IPlayniteAPI PlayniteApi { get; }
+        protected override string TargetUrl => "https://steamcommunity.com/my/edit/info";
 
         public SteamCommunityService(IPlayniteAPI playniteApi)
+            : base(playniteApi) { }
+
+        protected override async Task<SteamUserToken?> GetSteamUserTokenFromWebViewAsync(IWebView webView)
         {
-            PlayniteApi = playniteApi;
-        }
-        
-        public SteamUserToken GetAccessToken()
-        {
-            using (var view = PlayniteApi.WebViews.CreateOffscreenView())
+            var url = webView.GetCurrentAddress();
+            if (url.Contains("/login"))
+                throw new Exception(PlayniteApi.Resources.GetString(LOC.SteamNotLoggedInError));
+
+            var source = await webView.GetPageSourceAsync();
+            var userIdMatch = Regex.Match(source, @"g_steamID = ""(?<id>[0-9]+)""");
+            var tokenMatch = Regex.Match(source, "&quot;webapi_token&quot;:&quot;(?<token>[^&]+)&quot;");
+
+            if (!userIdMatch.Success || !tokenMatch.Success)
+                throw new Exception("Could not find Steam user ID or token");
+
+            return new SteamUserToken
             {
-                view.NavigateAndWait("https://steamcommunity.com/my/edit/info");
-                var url = view.GetCurrentAddress();
-                if (url.Contains("/login"))
-                    throw new Exception(PlayniteApi.Resources.GetString(LOC.SteamNotLoggedInError));
-                    
-                var source = view.GetPageSource();
-                var userIdMatch = Regex.Match(source, @"g_steamID = ""(?<id>[0-9]+)""");
-                var tokenMatch = Regex.Match(source, "&quot;webapi_token&quot;:&quot;(?<token>[^&]+)&quot;");
-                
-                if (!userIdMatch.Success || !tokenMatch.Success)
-                    throw new Exception("Could not find Steam user ID or token");
-                
-                return new SteamUserToken
-                {
-                    UserId = ulong.Parse(userIdMatch.Groups["id"].Value),
-                    AccessToken =  tokenMatch.Groups["token"].Value,
-                };
-            }
+                UserId = ulong.Parse(userIdMatch.Groups["id"].Value),
+                AccessToken = tokenMatch.Groups["token"].Value,
+            };
         }
     }
 }
