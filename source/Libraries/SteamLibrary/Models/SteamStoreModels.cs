5af9378ff6d10c35fd5688ef222cf3502d5f303e
Jeshibu <37979717+Jeshibu@users.noreply.github.com>
2026-07-27 17:28:19 +0200

    Steam: use HTML parsing instead of regex for token (#539)
    Also more logging

--- source/Libraries/SteamLibrary/Models/SteamStoreModels.cs
+++ source/Libraries/SteamLibrary/Models/SteamStoreModels.cs
@@ -7,7 +7,25 @@ namespace SteamLibrary.Models
 
     public struct SteamUserToken
     {
-        public ulong UserId;
-        public string AccessToken;
+        public SteamUserToken(ulong userId, string accessToken)
+        {
+            UserId = userId;
+            AccessToken = accessToken;
+        }
+
+        public readonly ulong UserId;
+        public readonly string AccessToken;
+    }
+
+    public class StoreUserConfig
+    {
+        public string webapi_token { get; set; }
+    }
+
+    public class UserInfo
+    {
+        public bool logged_in { get; set; }
+        public string steamid { get; set; }
+        public string account_name { get; set; }
     }
 }
